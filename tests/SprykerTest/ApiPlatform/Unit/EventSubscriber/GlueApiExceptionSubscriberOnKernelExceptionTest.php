f57e5750697132ad6f50e913ff2fe4e9c3d100a6
Olha Livitchuk <77281282+olhalivitchuk@users.noreply.github.com>
2026-06-01 15:10:56 +0300

    CC-39010 Fixes for Acceptance compatibility. (#681)
    CC-39010  Fixes for Acceptance and Fuctional compatibility tests.

--- tests/SprykerTest/ApiPlatform/Unit/EventSubscriber/GlueApiExceptionSubscriberOnKernelExceptionTest.php
+++ tests/SprykerTest/ApiPlatform/Unit/EventSubscriber/GlueApiExceptionSubscriberOnKernelExceptionTest.php
@@ -194,7 +194,7 @@ class GlueApiExceptionSubscriberOnKernelExceptionTest extends Unit
         return new GlueApiExceptionSubscriber(
             $this->createMock(TranslatorInterface::class),
             $this->createMock(ResourceMetadataCollectionFactoryInterface::class),
-            true
+            true,
         );
     }
 
